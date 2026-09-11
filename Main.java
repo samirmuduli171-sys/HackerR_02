993f20f8d3634f20aa0791fafd0a70db571b04b4
samirmuduli171-sys <smuduli171@gmail.com>
2026-09-11 12:46:25 +0530

    to print some lines

---
+++ Main.java
@@ -0,0 +1,17 @@
+package HackerR_02;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double pi = sc.nextDouble();
+        sc.nextLine(); // Consume the remaining newline
+        String sen = sc.nextLine();
+        System.out.println("String: " + sen);
+        System.out.println("Double: " + pi);
+        System.out.println("Int: " + n);
+
+    }
+}
